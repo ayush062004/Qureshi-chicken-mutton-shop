a31941b21fb82a03d283ac694ece23d240a79384
ayush062004 <CHANDAYUSH062004@GMAIL.COM>
2026-02-21 11:41:09 +0530

    mobile number update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     <footer className="footer">
       <p>© {new Date().getFullYear()} Qureshi Mutton & Chicken Shop</p>
       <p>📍 Mohhadipur in front of rkbk Maruti showroom</p>
-      <p>📞 9876543210</p>
+      <p>📞  6393594508</p>
       <p>⏰ Open Daily: 7 AM – 10 PM</p>
     </footer>
   );
